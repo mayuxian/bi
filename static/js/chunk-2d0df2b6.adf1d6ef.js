96f3c9095feb6e1b96eb2ed7011cc14b1a9c7b48
mayuxian <mayuxian@autohome.com.cn>
2021-03-17 20:28:24 +0800

    升级更新

--- static/js/chunk-2d0df2b6.adf1d6ef.js
+++ static/js/chunk-2d0df2b6.adf1d6ef.js
@@ -1,2 +1,1 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0df2b6"],{8987:function(e,r,n){"use strict";n.r(r);n("5319"),n("ac1f");var t,u,a={created:function(){var e=this.$route,r=e.params,n=e.query,t=r.path;this.$router.replace({path:"/"+t,query:n})},render:function(e){return e()}},c=a,o=n("2877"),p=Object(o["a"])(c,t,u,!1,null,null,null);r["default"]=p.exports}}]);
-//# sourceMappingURL=chunk-2d0df2b6.adf1d6ef.js.map
